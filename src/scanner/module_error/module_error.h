db6e0ae507a4bcd67a309c1f2030607d13d93ee6
Gorka Hernandez Villalon <131766898+Gorkah@users.noreply.github.com>
2026-02-09 21:10:26 +0100

    error.h init functions
    error.h init functions

--- src/scanner/module_error/module_error.h
+++ src/scanner/module_error/module_error.h
@@ -1,11 +1,24 @@
-#ifndef MODULE1_H
-#define MODULE1_H
-
-#include "../main.h"
-
-#define FIBNUM 4 // A number to use in real program (not for testing)
-
-int fib(int n);
-
-#endif
- 
+#ifndef MODULE_ERROR_H
+#define MODULE_ERROR_H
+#include "../config.h"
+// Maximum number of error/warning entries stored
+#define MAX_ERRORS 256
+// Error severity levels
+typedef enum {
+    SEVERITY_WARNING,
+    SEVERITY_ERROR
+} Severity;
+// Single error/warning entry
+typedef struct {
+    char message[MAX_LINE_LENGTH];
+    int line;
+    Severity severity;
+} ErrorEntry;
+void error_init(void);
+void report_error(const char* message, int line);
+void report_warning(const char* message, int line);
+void report_error_token(const Token* token, int line);
+int error_count(void);
+int warning_count(void);
+void error_finalize(void);
+#endif // MODULE_ERROR_H
