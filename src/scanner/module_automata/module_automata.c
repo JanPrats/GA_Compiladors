8b46c76c0b97285927d2910524fc2aae11916960
Andrea <131747892+andreasallo@users.noreply.github.com>
2026-02-10 18:26:50 +0100

    correccio num automata, sense funcio

--- src/scanner/module_automata/module_automata.c
+++ src/scanner/module_automata/module_automata.c
@@ -38,38 +38,31 @@ AutomataDFA return_automata = {
 };
 
 
-AutomataDFA automata_number_create(void) {
-    AutomataDFA a;
-    memset(&a, 0, sizeof(a));
+AutomataDFA number_automata = {
+    .alphabet = { '0','1','2','3','4','5','6','7','8','9' },
+    .states = { 1, 2, 3 },        
+    .start_state = 1,
+    .current_state = 1,
+    .accepting_states = { 2 },
+    .lookahead_acceptance = {
+        ' ', '\t', '\n',
+        ';', ',', '.', 
+        '(', ')', '{', '}', '[', ']',
+        '+', '=', '*', '>'
+    },
 
-    a.type = CAT_NUMBER;
-
-    //Alphabet
-    strcpy(a.alphabet, "0123456789");
-    a.matrix.width = 10;
-
-    //States: 1 init, 2 accepting, 3 sink
-    set_int_list_end(a.states, MAX_STATES);
-    a.states[0] = 1;
-    a.states[1] = 2;
-    a.states[2] = 3;
-    a.matrix.height = 3;
-    a.start_state = 1;
-    a.current_state = a.start_state;
-    set_int_list_end(a.accepting_states, MAX_STATES);
-    a.accepting_states[0] = 2;
-
-    // Lookahead acceptance: delimitadores típicos tras número
-    strcpy(a.lookahead_acceptance, " \t\n;,.(){}[]+=*>");
-    a.dont_look_anymore = false;
-
-    for (int col = 0; col < a.matrix.width; col++) {
-        a.matrix.states_rows[0].new_state[col] = 2; // from state 1
-        a.matrix.states_rows[1].new_state[col] = 2; // from state 2
-        a.matrix.states_rows[2].new_state[col] = 3; // from state 3
-    }
-    return a;
-}
+    .matrix = {
+        .states_rows = {
+            { .new_state = { 2,2,2,2,2,2,2,2,2,2 } },
+            { .new_state = { 2,2,2,2,2,2,2,2,2,2 } },
+            { .new_state = { 3,3,3,3,3,3,3,3,3,3 } }
+        },
+        .width  = 10,
+        .height = 3
+    },
+    .type = CAT_NUMBER,
+    .dont_look_anymore = false
+};
 
 
 AutomataDFA specials_automata = {
